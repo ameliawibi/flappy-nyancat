29f7fb791bf5332cba40f51b1d4a525da9408486
Amelia Wibisono <ameliawibi@gmail.com>
2022-06-12 16:11:59 +0800

    fix socket

--- socket/client.js
+++ socket/client.js
@@ -2,4 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io();
 
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
+
 export default socket;
--- socket/server.js
+++ socket/server.js
@@ -1,16 +1,27 @@
+import { user } from "pg/lib/defaults";
+
 export default function init(io) {
   io.on("connection", (socket) => {
     console.log("a user is connected");
     console.log("socket id: ", socket.id);
 
+    //listing all users
+    const users = [];
+    for (let [id] of io.of("/").sockets) {
+      users.push({
+        userID: id,
+      });
+    }
+    socket.broadcast.emit("users", users);
+
     let gameRoom = "gameRoom";
 
     socket.on("subscribe", async () => {
-      let gameHistory = { data: "player" };
+      let userSocketId = { userID: users[users.length - 1].userID };
       socket.join(gameRoom);
-      console.log("a user has joined our room: " + gameRoom);
+      console.log("a user has joined our room: " + userSocketId);
 
-      io.to(gameRoom).emit("joinRoom", gameHistory);
+      io.to(gameRoom).emit("joinRoom", userSocketId);
     });
   });
 }
